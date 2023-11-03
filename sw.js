8be1e847ea38c0160744480f1e110413edc3a370
hemaaanth <hemaaanth@users.noreply.github.com>
2023-11-03 14:41:20 +0000

    deploy: 347def036d5afa1bb54b38bdae9f8e00143c60fc

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 //! IF ANYTHING IN THIS FILE IS CHANGED MAKE SURE setVersion.js HAS ALSO BEEN UPDATED
 // Increase the v number when the app is updated
-const version = "1.6.2";
+const version = "1.6.3";
 const cacheName = `joserdle-v${version}`;
 
 const assetsToCache = [
